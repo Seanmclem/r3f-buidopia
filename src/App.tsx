25d188b02fc3540610e3b3a887fdddc8a29b16a5
Seanmclem <seanmclements@gmail.com>
2023-07-14 22:39:44 -0400

    what what

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <p>I'm going to be something, someday.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ❤️ React
         </a>
       </header>
     </div>
